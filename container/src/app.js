37fa7c0873358eb2f355d79010a35e856958269f
Levan Tskipuri <ltskipuri@emis.ge>
2020-11-26 14:03:16 +0400

    update yml

--- container/src/app.js
+++ container/src/app.js
@@ -4,7 +4,7 @@ import Marketing from "./components/marketing";
 export default () => {
   return (
     <div>
-      <h1>Title</h1>
+      <h1>Title!</h1>
       <hr />
       <Marketing></Marketing>
     </div>
