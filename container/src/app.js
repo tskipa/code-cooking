9371e32849086636c94a65d185ac07bfe7fef181
Levan Tskipuri <ltskipuri@emis.ge>
2020-11-26 15:12:15 +0400

    add header to container

--- container/src/app.js
+++ container/src/app.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+import Header from "./components/header";
 import Marketing from "./components/marketing";
 
 export default () => {
   return (
-    <div>
-      <h1>Title!</h1>
-      <hr />
-      <Marketing></Marketing>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header></Header>
+        <Marketing></Marketing>
+      </div>
+    </BrowserRouter>
   );
 };
